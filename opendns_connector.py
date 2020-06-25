0b774d8b1890582ed298ff1d71bf13539b15267b
Jessica Hou <jhou@splunk.com>
2020-06-25 13:29:55 -0700

    PAPP-5058 ip reputation fails with no data

--- opendns_connector.py
+++ opendns_connector.py
@@ -311,7 +311,8 @@ class OpendnsConnector(BaseConnector):
             status_desc = STATUS_DESC['0']  # UNKNOWN
 
         if (not response):
-            return action_result.set_status(phantom.APP_ERROR, "Response does not contain any data")
+            response = ""
+            return action_result.set_status(phantom.APP_SUCCESS, "Response does not contain any data")
 
         try:
             block_domains = len(response)
